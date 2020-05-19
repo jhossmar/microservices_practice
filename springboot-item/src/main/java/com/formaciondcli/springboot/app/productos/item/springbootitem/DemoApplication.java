36f5cdba5c5e5086e628e85b0e9b8d5d6d87dfba
jhossmar <jhosmar_ramos@hotmail.com>
2020-05-19 01:58:36 -0400

    config eureka cliente on services

--- springboot-item/src/main/java/com/formaciondcli/springboot/app/productos/item/springbootitem/DemoApplication.java
+++ springboot-item/src/main/java/com/formaciondcli/springboot/app/productos/item/springbootitem/DemoApplication.java
@@ -2,11 +2,10 @@ package com.formaciondcli.springboot.app.productos.item.springbootitem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-
-@RibbonClient(name = "servicio-producto")
+@EnableEurekaClient
 @EnableFeignClients 
 @SpringBootApplication
 public class DemoApplication {
