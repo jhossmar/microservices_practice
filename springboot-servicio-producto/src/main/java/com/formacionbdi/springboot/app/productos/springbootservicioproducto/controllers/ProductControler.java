a2b9a887834713d299a840c5a9e152f0de85a0f3
jhossmar <jhosmar_ramos@hotmail.com>
2020-06-28 18:53:28 -0400

    add control of timeout to product-service

--- springboot-servicio-producto/src/main/java/com/formacionbdi/springboot/app/productos/springbootservicioproducto/controllers/ProductControler.java
+++ springboot-servicio-producto/src/main/java/com/formacionbdi/springboot/app/productos/springbootservicioproducto/controllers/ProductControler.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductControler {
-    
+
     @Autowired
     private Environment env;
 
@@ -24,26 +24,33 @@ public class ProductControler {
 
     @Autowired
     private IProductoService productoService;
-    
+
     @GetMapping("/listar")
-    public List<Producto> listar(){
-        return productoService.findALL().stream().map(producto->{
-            //producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-            producto.setPort((int)port);
+    public List<Producto> listar() {
+        return productoService.findALL().stream().map(producto -> {
+            // producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+            producto.setPort((int) port);
             return producto;
         }).collect(Collectors.toList());
-    
+
     }
-    
+
     @GetMapping("/ver/{id}")
-    public Producto detalle(@PathVariable Long id) throws Exception{
-        Producto producto =productoService.findById(id);
-        //producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-        producto.setPort((int)port);
+    public Producto detalle(@PathVariable Long id) {// throws Exception{
+        Producto producto = productoService.findById(id);
+        // producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+        producto.setPort((int) port);
         boolean ok = false;
-        if (ok== false){
-            throw new Exception("No se pudo carga el producto");
+        // if (ok== false){
+        // throw new Exception("No se pudo carga el producto");
+        // }
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+
         return producto;
     }
     
