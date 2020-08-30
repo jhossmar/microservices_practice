b0ce1ebb8e6f21c50298bac0cc2b9c71a5c70104
jhossmar <jhosmar_ramos@hotmail.com>
2020-08-29 22:21:16 -0400

    add  config service to item-service

--- springboot-item/src/main/java/com/formaciondcli/springboot/app/productos/item/springbootitem/controllers/ItemController.java
+++ springboot-item/src/main/java/com/formaciondcli/springboot/app/productos/item/springbootitem/controllers/ItemController.java
@@ -1,14 +1,22 @@
 package com.formaciondcli.springboot.app.productos.item.springbootitem.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.formaciondcli.springboot.app.productos.item.springbootitem.models.entity.Item;
 import com.formaciondcli.springboot.app.productos.item.springbootitem.models.entity.Producto;
 import com.formaciondcli.springboot.app.productos.item.springbootitem.models.service.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +24,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ItemController {
     
+private static Logger log = LoggerFactory.getLogger(ItemController.class);
+
     @Autowired
     @Qualifier("itemServiceFein")
     //@Qualifier("itemServiceRestTempleate") //to change Rest Template
     public ItemService itemService;
+
+    @Value("${configuracion.text}")
+    private String texto;
 
     @GetMapping("/listar")
     public List<Item> listar(){
@@ -50,6 +63,15 @@ public class ItemController {
         item.setProducto(producto);
         return item;
 
+    }
+
+    @GetMapping("/obtener-config")
+    public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+        log.info(texto);
+        Map<String, String > json = new HashMap<>();
+        json.put("texto",texto);
+        json.put("puerto",puerto);
+        return new ResponseEntity<Map<String, String >>(json,HttpStatus.OK);
     }
 
 }
